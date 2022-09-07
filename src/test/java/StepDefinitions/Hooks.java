ccf563fd6ffd55799b47bc78fbb1d1962db11dc1
alliswell <koray.akcay55@gmail.com>
2022-09-07 04:28:17 +0300

    Error Screenshot

--- src/test/java/StepDefinitions/Hooks.java
+++ src/test/java/StepDefinitions/Hooks.java
@@ -3,8 +3,14 @@ package StepDefinitions;
 import Utils.Driver;
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
+import io.cucumber.java.Scenario;
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 
+import java.io.File;
+import java.io.IOException;
 import java.time.Duration;
 
 public class Hooks {
@@ -19,7 +25,25 @@ public class Hooks {
     }
 
     @After
-    public void tearDown() {
+    public void tearDown(Scenario scenario) {
+        if (scenario.isFailed()){
+
+            TakesScreenshot screenshot = ((TakesScreenshot) driver); // Which driver to get a screenshot
+
+            File srcFile = screenshot.getScreenshotAs(OutputType.FILE);
+
+            String scenarioName = scenario.getName();
+
+            File destinationFile = new File("target/FailedScreenshots/" + scenarioName + ".png");
+
+            try {
+                FileUtils.copyFile(srcFile, destinationFile);
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+
+        }
+
         Driver.quitDriver();
     }
 
